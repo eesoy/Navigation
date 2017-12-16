6101de16bd18be43ffcbce1277a402737b83ddae
SO YOUNG <LEESOYOUNG@ITui-MacBook-Pro.local>
2017-12-16 16:56:32 +0900

    세그로 이동, 버튼으로 이동, 다른 스토리보드 뷰 컨트롤러로 이동

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -12,14 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func goPage2(_ sender: UIButton) {
+//        let vc = Page2ViewController()
+//        self.navigationController?.pushViewController(vc, animated: true)
+        //optional 오류 방지 래핑
+        if let pc =  self.storyboard?.instantiateViewController(withIdentifier: "Page2View") {
+            navigationController?.pushViewController(pc, animated: true)
+        }
+        
+    }
+    
+    @IBAction func goPage2WithSegue(_ sender: Any) {
+        performSegue(withIdentifier: "goToPage2Segue", sender: self)
+    }
+    
+    @IBAction func goPage4WithAnotherStoryBoard(_ sender: UIButton) {
+        let page4StoryBoard = UIStoryboard(name: "Page4Storyboard", bundle: nil)
+        
+        let pc4 = page4StoryBoard.instantiateViewController(withIdentifier: "Page4View")
+        navigationController?.pushViewController(pc4, animated: true)
+        
+        
+    }
 }
 
